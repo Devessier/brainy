2ef7281b65e25822b243737ae8968f956e20c1f7
Devessier <baptiste@devessier.fr>
2021-06-22 19:14:35 +0200

    docs: create a simple example using an on/off state machine

---
+++ examples/on-off/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Devessier/brainy"
+)
+
+func main() {
+	const (
+		OnState  brainy.StateType = "on"
+		OffState brainy.StateType = "off"
+
+		OnEvent  brainy.EventType = "on"
+		OffEvent brainy.EventType = "off"
+	)
+
+	onOffMachine := brainy.Machine{
+		Initial: OffState,
+
+		StateNodes: brainy.StateNodes{
+			OnState: brainy.StateNode{
+				On: brainy.Events{
+					OffEvent: OffState,
+				},
+			},
+			OffState: brainy.StateNode{
+				On: brainy.Events{
+					OnEvent: OnState,
+				},
+			},
+		},
+	}
+	onOffMachine.Init()
+
+	initialState := onOffMachine.Current() // off
+	fmt.Printf("initial state of the state machine is: %s\n", initialState)
+
+	onOffMachine.Send(OnEvent)
+
+	stateAfterOnEvent := onOffMachine.Current() // on
+	fmt.Printf("state of the state machine after receiving an on event from off state is: %s\n", stateAfterOnEvent)
+
+	onOffMachine.Send(OnEvent)
+
+	stateAfterASecondOnEvent := onOffMachine.Current() // on
+	fmt.Printf("state of the state machine after receiving an on event from on state is: %s\n", stateAfterASecondOnEvent)
+}
