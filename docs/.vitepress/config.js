820b6b9d34d372e2d3e55613081cd250dec41789
Devessier <baptiste@devessier.fr>
2021-06-22 23:36:02 +0200

    docs: configure pages to show in sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,20 @@
 module.exports = {
   title: "brainy",
   description: "brainy online documentation",
+
+  themeConfig: {
+    sidebar: {
+      '/': [
+        {
+          text: 'Introduction',
+          children: [
+            {
+              text: 'Tour of brainy',
+              link: '/tour-of-brainy'
+            },
+          ]
+        }
+      ]
+    }
+  }
 };
